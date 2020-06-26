15fe552ae2a204489d1da23e91cac9d1d5d895dd
Mohammad Ali Toufighi <mohammadali.toufighi@tap30co.com>
2020-06-26 12:49:14 +0430

    add convolutional encoder

---
+++ ConvolutionalEncoder.py
@@ -0,0 +1,20 @@
+class ConvolutionalEncoder:
+    _mappings = {
+        "100": "11",
+        "000": "00",
+        "110": "00",
+        "010": "11",
+        "111": "10",
+        "011": "01",
+        "101": "01",
+        "001": "10"
+    }
+    
+    @staticmethod
+    def encode(binary_string):
+        result = ""
+        shift_register = '00'
+        for bit in binary_string:
+            result += ConvolutionalEncoder._mappings[f'{bit}{shift_register}']
+            shift_register = f'{bit}{shift_register[0]}'
+        return result
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from utils import *
 from HuffmanEncoder import HuffmanEncoder
 from HuffmanDecoder import HuffmanDecoder
+from ConvolutionalEncoder import ConvolutionalEncoder
 
 probabilities = read_probabilites()
 he = HuffmanEncoder(probabilities)
 hd = HuffmanDecoder(probabilities)
 
-res = he.encode("alitou")
-res = hd.decode(res)
+res = he.encode("a")
+print(res)
+res = ConvolutionalEncoder.encode(res)
+# res = hd.decode(res)
 print(res)
