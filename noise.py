9934027c3c887be631f3bf80dcb947993e84747d
Mohammad Ali Toufighi <mohammadali.toufighi@tap30co.com>
2020-06-26 15:00:05 +0430

    reduce noice to test better

--- noise.py
+++ noise.py
@@ -5,7 +5,7 @@ def noise(data):
     result = ""
     for i in range(len(data)):
         r = np.random.rand()
-        if r < 0.1:
+        if r < 0.01:
             result += '1' if data[i] == '0' else '1'
         else:
             result += data[i]
